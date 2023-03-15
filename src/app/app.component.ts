20eacecd28241644153cdc089e7df1e2b99d20cd
Miguel Malqui <miquelangel9924@gmail.com>
2023-03-15 10:32:19 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Calculator } from 'miguel-malqui-calculator';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator-angular';
+  title = 'Angular Calculator';
+
+  calculator: Calculator = new Calculator();
+
+  operandA: number = 0;
+  operandB: number = 0;
+  result: number = 0;
+
+  add(): void {
+    this.result = this.calculator.add(this.operandA, this.operandB);
+  }
+
+  subtract(): void {
+    this.result = this.calculator.subtract(this.operandA, this.operandB);
+  }
+
+  multiply(): void {
+    this.result = this.calculator.multiply(this.operandA, this.operandB);
+  }
+
+  divide(): void {
+    this.result = this.calculator.divide(this.operandA, this.operandB);
+  }
 }
